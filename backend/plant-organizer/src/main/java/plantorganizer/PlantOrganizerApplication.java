6d33f6f714e1234f2471b5295e3f3deb3c1f6968
tjncc <55355686+tjncc@users.noreply.github.com>
2020-07-27 16:59:36 +0200

    initialized backend app

---
+++ backend/plant-organizer/src/main/java/plantorganizer/PlantOrganizerApplication.java
@@ -0,0 +1,13 @@
+package plantorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantOrganizerApplication.class, args);
+	}
+
+}
---
+++ backend/plant-organizer/src/test/java/plantorganizer/PlantOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package plantorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
